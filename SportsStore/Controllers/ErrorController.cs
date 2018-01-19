d9cc37a6eb577e23a548c2706b6f6980bc374a34
Naniks <nanevokshonov@mail.ru>
2018-01-19 23:01:58 +0300

    create project

---
+++ SportsStore/Controllers/ErrorController.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SportsStore.Controllers {
+    public class ErrorController : Controller {
+        public ViewResult Error() => View();
+    }
+}
---
+++ SportsStore/MyOwnWrapContainer/WrapContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.MyOwnWrapContainer {
+    public class WrapContainer<T> {
+        public List<T> lineCollection = new List<T>();
+    }
+}
